65f53924cb237f89b4a29980c0c5dc364f02d66d
Rogerio Ricardo <rrssouza@yahoo.com.br>
2025-10-26 21:34:44 -0300

    database update

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     MYSQL_HOST = os.environ.get('MYSQL_HOST', 'rrssouza.mysql.pythonanywhere-services.com')
     MYSQL_USER = os.environ.get('MYSQL_USER', 'rrssouza')
     MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '02090702Sou*')
-    MYSQL_DB = os.environ.get('MYSQL_DB', 'rrssouza$dbgestaoteen')
+    MYSQL_DB = os.environ.get('MYSQL_DB', 'rrssouza$rrssouzadbgestaoteen')
 
     # Definir diretamente a URI de conexão como uma string
     SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}'
